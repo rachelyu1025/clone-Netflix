d2113c940aafd5461f1c487d37a7d1eaad043e01
EUNBI <rachelyu.1025@gmail.com>
2024-08-28 18:33:38 +0900

    ✨  route 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './Layout/AppLayout';
+import Home from './pages/Home';
+import Movies from './pages/Movies/index';
+import MovieDetail from './pages/MovieDetail/index';
 
 function App() {
-  return <div className=''></div>;
+  return (
+    <Routes>
+      <Route path='/' element={AppLayout}>
+        <Route index element={Home} />
+        <Route path='movies'>
+          <Route index element={Movies} />
+          <Route path=':id' element={MovieDetail} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
